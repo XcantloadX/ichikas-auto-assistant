80447e57ba89879a44ffa9966930f5ac876daa64
XcantloadX <3188996979@qq.com>
2025-09-09 21:30:10 +0800

    fix(task): 修复挑战演出只能使用星乃一歌的问题

--- iaa/tasks/live/challenge_live.py
+++ iaa/tasks/live/challenge_live.py
@@ -1,10 +1,10 @@
 from kotonebot import task
 
-from .live import challenge_live as do_challenge_live
-from iaa.config.schemas import GameCharacter
+from iaa.context import conf
 from iaa.tasks.start_game import go_home
+from .live import challenge_live as do_challenge_live
 
 @task('挑战演出')
 def challenge_live():
     go_home()
-    do_challenge_live(GameCharacter.Ichika)
+    do_challenge_live(conf().challenge_live.characters[0])
